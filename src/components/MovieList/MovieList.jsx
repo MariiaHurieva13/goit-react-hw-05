66358aa9dab533aed4391c4237a544816609fc18
MariiaHurieva13 <Marygureva@gmail.com>
2025-04-21 20:30:33 +0300

    Create MovieList.jsx

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,24 @@
+import { Link, useLocation } from "react-router-dom";
+
+const MovieList = ({ movies }) => {
+  const location = useLocation();
+  return (
+    <>
+      {movies.length === 0 ? (
+        <p>Movie not found</p>
+      ) : (
+        <ul>
+          {movies.map((movie) => (
+            <li key={movie.id}>
+              <Link to={`/movies/${movie.id}`} state={location}>
+                {movie.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
+
+export default MovieList;
